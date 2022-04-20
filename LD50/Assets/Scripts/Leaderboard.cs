3c12f79aa3b40637af876c696f82a33ca71b61fa
Cliff Lee CL <tommyboys0107@gmail.com>
2022-04-20 22:55:50 +0800

    [Modify] Add player best score mechanism.

--- LD50/Assets/Scripts/Leaderboard.cs
+++ LD50/Assets/Scripts/Leaderboard.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using LootLocker.Requests;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Leaderboard : MonoBehaviour
 {
     [SerializeField] TMP_Text playerRecordText;
+    [SerializeField] TMP_Text playerBestRecordText;
     [SerializeField] TMP_InputField playerNameInput;
     [SerializeField] Button submitBtn;
+    [SerializeField] Button restartBtn;
+    [SerializeField] Button exitBtn;
     [SerializeField] Button refreshBtn;
     [SerializeField] List<TMP_Text> topRecordTextList = new List<TMP_Text>();
     [SerializeField] int leaderBoardID;
     [SerializeField] int topRecordCount = 10;
 
+    int PlayerCurrentRecordTicks => (int)TimeSpan.FromSeconds(GameManager.Instance.roundTimer.CurrentTime).Ticks; 
+    int PlayerBestRecordTicks => PlayerPrefs.GetInt("PlayerBestRecord", 0);
     bool isWaitingSubmitResponse = false;
     
     void Start()
     {
         StartGuestSession();
         submitBtn.onClick.AddListener(OnSubmitClicked);
+        restartBtn.onClick.AddListener(OnRestartClicked);
+        exitBtn.onClick.AddListener(OnExitClicked);
         refreshBtn.onClick.AddListener(OnRefreshClicked);
         playerNameInput.onSubmit.AddListener(SubmitScore);
     }
@@ -29,7 +37,10 @@ public class Leaderboard : MonoBehaviour
     void OnEnable()
     {
         UpdateLeaderboard();
+        if (PlayerCurrentRecordTicks > PlayerBestRecordTicks)
+            PlayerPrefs.SetInt("PlayerBestRecord", PlayerCurrentRecordTicks);
         playerRecordText.text = TimeSpan.FromSeconds(GameManager.Instance.roundTimer.CurrentTime).ToString("mm:ss.ff");
+        playerBestRecordText.text = $"Best: {@TimeSpan.FromTicks(PlayerBestRecordTicks):mm\\:ss\\.ff}";
     }
 
     void OnSubmitClicked()
@@ -38,11 +49,26 @@ public class Leaderboard : MonoBehaviour
             return;
 
         SubmitScore(playerNameInput.text);
+        AudioManager.Instance.PlaySound(AudioManager.AudioName.ButtonClicked);
+    }
+    
+    void OnRestartClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        AudioManager.Instance.PlaySound(AudioManager.AudioName.ButtonClicked);
+    }
+    
+    void OnExitClicked()
+    {
+        Application.Quit();
+        Debug.Log("Exit game!");
+        AudioManager.Instance.PlaySound(AudioManager.AudioName.ButtonClicked);
     }
 
     void OnRefreshClicked()
     {
         UpdateLeaderboard();
+        AudioManager.Instance.PlaySound(AudioManager.AudioName.ButtonClicked);
     }
 
     void StartGuestSession()
@@ -69,7 +95,7 @@ public class Leaderboard : MonoBehaviour
             {
                 for (var i = 0; i < topRecordTextList.Count; i++)
                     if (i == 0)
-                        topRecordTextList[i].text = "Network error! Please try ro refresh.";
+                        topRecordTextList[i].text = "Network error! Please try again.";
                     else
                         topRecordTextList[i].text = $"{i}. ??:??.?? - No Record.";
             }
@@ -79,11 +105,8 @@ public class Leaderboard : MonoBehaviour
 
     void SubmitScore(string inputStr)
     {
-        var score = (int) TimeSpan.FromSeconds(GameManager.Instance.roundTimer.CurrentTime).Ticks;
-
-        Debug.Log($"Save {GameManager.Instance.roundTimer.CurrentTime} to leaderboard.");
         isWaitingSubmitResponse = true;
-        LootLockerSDKManager.SubmitScore(inputStr, score, leaderBoardID, (response) =>
+        LootLockerSDKManager.SubmitScore(inputStr, PlayerBestRecordTicks, leaderBoardID, (response) =>
         {
             if (response.success)
             {
